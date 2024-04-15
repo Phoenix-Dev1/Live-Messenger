fc1c193b40930e6522fd8d19450f232d16900ab4
Phoenix-Dev <116443234+Phoenix-Dev1@users.noreply.github.com>
2024-04-15 04:34:37 +0300

    video conference - room creation page(beta)

--- app/video-conference/page.tsx
+++ app/video-conference/page.tsx
@@ -3,38 +3,79 @@
 import "@livekit/components-styles";
 import {
   LiveKitRoom,
-  VideoConference,
   GridLayout,
   ParticipantTile,
   RoomAudioRenderer,
   ControlBar,
   useTracks,
 } from "@livekit/components-react";
-import { Track } from "livekit-client";
 import { useEffect, useState } from "react";
+import { Track } from "livekit-client";
+import { useSearchParams } from "next/navigation";
 
 export default function Page() {
-  // TODO: get user input for room and name
-  const room = "quickstart-room";
-  const name = "quickstart-user";
-  const [token, setToken] = useState("");
+  const params = useSearchParams();
 
   useEffect(() => {
-    (async () => {
-      try {
-        const resp = await fetch(
-          `/api/get-participant-token?room=${room}&username=${name}`
-        );
-        const data = await resp.json();
-        setToken(data.token);
-      } catch (e) {
-        console.error(e);
-      }
-    })();
-  }, []);
+    const room = params!.get("room");
+    const name = params!.get("name");
+    if (room && name) {
+      setRoom(room);
+      setName(name);
+    }
+  }, [params]);
+
+  const [room, setRoom] = useState<string>();
+  const [name, setName] = useState<string>();
+
+  const [token, setToken] = useState("");
+
+  async function getToken() {
+    if (!room || !name) {
+      return;
+    }
+    try {
+      const resp = await fetch(
+        `/api/get-participant-token?room=${room}&username=${name}`
+      );
+      const data = await resp.json();
+      setToken(data.token);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   if (token === "") {
-    return <div>Getting token...</div>;
+    return (
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          getToken();
+        }}
+        className="flex flex-col justify-center items-center min-h-screen"
+      >
+        <input
+          type="text"
+          placeholder="Room"
+          value={room}
+          className="mb-4 ring-1 ring-gray-300"
+          onChange={(e) => setRoom(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          className="mb-4 ring-1 ring-gray-300"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors"
+        >
+          Join
+        </button>
+      </form>
+    );
   }
 
   return (
@@ -43,6 +84,7 @@ export default function Page() {
       audio={true}
       token={token}
       serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+      onDisconnected={() => setToken("")}
       // Use the default LiveKit theme for nice styles.
       data-lk-theme="default"
       style={{ height: "100dvh" }}
@@ -51,7 +93,7 @@ export default function Page() {
       <MyVideoConference />
       {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
       <RoomAudioRenderer />
-      {/* Controls for the user to start/stop audio, video, and screen
+      {/* Controls for the user to start/stop audio, video, and screen 
       share tracks and to leave the room. */}
       <ControlBar />
     </LiveKitRoom>
