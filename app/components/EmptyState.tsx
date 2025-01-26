126d336893a8702c02e74a37444dff9d2c9f3f77
Phoenix-Dev <116443234+Phoenix-Dev1@users.noreply.github.com>
2025-01-26 19:01:05 +0200

    [Fix] - Missing webpacks fixed

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -1,26 +1,24 @@
 import Image from "next/image";
-import getCurrentUser from "@/app/actions/getCurrentUser";
 
-const EmptyState = async () => {
-  const currentUser = await getCurrentUser(); // Wait for user data
+const EmptyState = () => {
   return (
     <div
       className="
-  px-4
-  py-10
-  sm:px-6
-  lg:px-8
-  h-full
-  flex
-  justify-center
-  items-center
-  bg-gray-100
-  "
+px-4
+py-10
+sm:px-6
+lg:px-8
+h-full
+flex
+justify-center
+items-center
+bg-gray-100
+"
     >
       <div className="text-center items-center flex flex-col">
         <Image alt="Avatar" src="/images/logo.png" width="400" height="400" />
         <p className="mt-4 text-xl font-semibold text-gray-800">
-          Welcome {currentUser?.name || "Guest"}!{" "}
+          Welcome To Live Messenger
         </p>
         <p className="mt-2 text-sm text-gray-600">
           Select a user from the list to begin your conversation.
